80a82e2bc818a9a6f08578ad3a4ab417aa80c059
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-06-26 16:49:14 +0200

    tutorials

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavBarComp/>
+  <router-view/>
+  <FooterComp/>
+</template>
+
+<script>
+import NavBarComp from './components/NavBar.vue';
+import FooterComp from './components/Footer.vue';
+
+
+export default {
+
+  components: {NavBarComp,FooterComp}
+
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <nav class="topnav">
+        <routerlink class="active" to="/">Home</routerlink>
+        <routerlink to="/"> Tutorials </routerlink>
+        <routerlink to="/"> About </routerlink>
+    </nav>
+
+</template>
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style scoped>
+
+.topnav {
+    background-color: #363636;
+    overflow: hidden;
+}
+
+routerlink{
+    float: left;
+    color: #fff;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 20px;
+}
+
+
+</style>
